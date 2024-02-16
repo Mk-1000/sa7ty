ccd98c467f15f38c3d02cf931b17db1b7a3df88e
Mokni Hamdi <86926622+Mk-1000@users.noreply.github.com>
2024-02-16 16:16:56 +0100

    add Consultation, prescreption, analyse entity and there relations

---
+++ src/Entity/Consultation.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConsultationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConsultationRepository::class)]
+class Consultation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $startAt = null;
+
+    #[ORM\Column]
+    private ?int $endAt = null;
+
+    #[ORM\OneToOne(inversedBy: 'consultation', cascade: ['persist', 'remove'])]
+    private ?Prescription $Prescription = null;
+
+    #[ORM\OneToMany(targetEntity: Analyse::class, mappedBy: 'consultation')]
+    private Collection $Analyses;
+
+    public function __construct()
+    {
+        $this->Analyses = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStartAt(): ?int
+    {
+        return $this->startAt;
+    }
+
+    public function setStartAt(int $startAt): static
+    {
+        $this->startAt = $startAt;
+
+        return $this;
+    }
+
+    public function getEndAt(): ?int
+    {
+        return $this->endAt;
+    }
+
+    public function setEndAt(int $endAt): static
+    {
+        $this->endAt = $endAt;
+
+        return $this;
+    }
+
+    public function getPrescription(): ?Prescription
+    {
+        return $this->Prescription;
+    }
+
+    public function setPrescription(?Prescription $Prescription): static
+    {
+        $this->Prescription = $Prescription;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Analyse>
+     */
+    public function getAnalyses(): Collection
+    {
+        return $this->Analyses;
+    }
+
+    public function addAnalysis(Analyse $analysis): static
+    {
+        if (!$this->Analyses->contains($analysis)) {
+            $this->Analyses->add($analysis);
+            $analysis->setConsultation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAnalysis(Analyse $analysis): static
+    {
+        if ($this->Analyses->removeElement($analysis)) {
+            // set the owning side to null (unless already changed)
+            if ($analysis->getConsultation() === $this) {
+                $analysis->setConsultation(null);
+            }
+        }
+
+        return $this;
+    }
+}
