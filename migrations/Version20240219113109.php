8499e2d256b5214fdeffa5c8bb353720283770c1
Amen Allah Jouila <amenallah1991@gmail.com>
2024-02-21 11:08:12 +0100

    Affichage HomePage et AboutUs

--- migrations/Version20240219113109.php
+++ migrations/Version20240219113109.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240217102803 extends AbstractMigration
+final class Version20240219113109 extends AbstractMigration
 {
     public function getDescription(): string
     {
