0f7aff603bc9081e2475402f26e830a486499f82
Oussama Fekih Hassen <hssanoussama@gmail.com>
2024-02-26 09:02:36 +0100

    correct the login

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -49,7 +49,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('LOGIN_ROUTE'));
+        return new RedirectResponse($this->urlGenerator->generate('app_login'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
